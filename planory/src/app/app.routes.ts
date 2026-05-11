3f88d70b973da350949aaa55e2bd4f7cef177a61
jordanEscobar <jordanescosoto@gmail.com>
2026-05-11 16:17:55 -0400

    Proyect structure

---
+++ planory/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'home',
+    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'add-task',
+    loadComponent: () => import('./pages/add-task/add-task.page').then( m => m.AddTaskPage)
+  },
+];
