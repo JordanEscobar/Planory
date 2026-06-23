f04c7d7be054ce65b1bab8c00665c8f7e8cfe218
jordanEscobar <jordanescosoto@gmail.com>
2026-06-22 22:29:59 -0400

    popup confirmación add-task

--- planory/src/app/pages/add-task/add-task.page.ts
+++ planory/src/app/pages/add-task/add-task.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent,IonButton, IonHeader, IonTitle, IonToolbar, IonItem, IonInput, IonBadge } from '@ionic/angular/standalone';
+import { IonContent,IonButton, IonHeader,IonAlert, IonTitle, IonToolbar, IonItem, IonInput, IonBadge } from '@ionic/angular/standalone';
 import {Task} from '../../core/models/task.model';
 import { TaskService } from 'src/app/core/services/taskService';
 
@@ -10,9 +10,11 @@ import { TaskService } from 'src/app/core/services/taskService';
   templateUrl: './add-task.page.html',
   styleUrls: ['./add-task.page.scss'],
   standalone: true,
-  imports: [IonBadge, IonInput, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonBadge, IonInput,IonAlert, IonButton, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class AddTaskPage implements OnInit {
+
+    public alertButtons: any[] = [];
 
     title = '';
     description = '';
@@ -23,6 +25,22 @@ export class AddTaskPage implements OnInit {
 
   ngOnInit() {
     this.tareasPCount = this.taskService.getPendingCount();
+
+    this.alertButtons = [
+    {
+      text: 'Cancelar',
+      role: 'cancel',
+    },
+    {
+      text: 'Aceptar',
+      handler: () => {
+        this.addTask();
+      }
+    }
+  ];
+
+
+
      }
 
     addTask(){
@@ -39,7 +57,10 @@ export class AddTaskPage implements OnInit {
       this.taskService.addTask(newTask);
       this.title = '';
       this.description = '';
-      alert('Tarea agregada exitosamente');
+      //alert('Tarea agregada exitosamente');
+    
+      
+      
     }
 
 }
