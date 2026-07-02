12eb216e311cd34d484a57bb68681247e61a5024
jordanEscobar <jordanescosoto@gmail.com>
2026-07-02 12:50:27 -0400

    Start estadisticas

--- planory/src/app/pages/estadisticas/estadisticas.page.ts
+++ planory/src/app/pages/estadisticas/estadisticas.page.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader,IonBadge, IonTitle, IonToolbar } from '@ionic/angular/standalone';
 import { TaskService } from 'src/app/core/services/taskService';
+import {Chart} from 'chart.js/auto';
+
 
 @Component({
   selector: 'app-estadisticas',
@@ -13,10 +15,30 @@ import { TaskService } from 'src/app/core/services/taskService';
 })
 export class EstadisticasPage implements OnInit {
   tareasPCount: number =0;
+  chart: any;
   constructor(private taskService: TaskService) { }
 
   ngOnInit() {
     this.tareasPCount = this.taskService.getPendingCount();
+    this.crearGrafico();
   }
+
+crearGrafico() {
+  if (this.chart) {
+    this.chart.destroy();
+  }
+
+  this.chart = new Chart('pieChart', {
+    type: 'pie',
+    data: {
+      labels: ['Pendientes', 'Completadas'],
+      datasets: [{
+        data: [5, 10]
+      }]
+    }
+  });
+}
+
+
 
 }
